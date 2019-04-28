25be89a7092b6567439eea4eefbcc52b8b05337c
Unknown <yzj999@gmail.com>
2019-04-28 19:21:11 -0400

    90-10 Automation Completed
    90-10**_auto.xlsx

--- renamingCSVs.py
+++ renamingCSVs.py
@@ -6,10 +6,10 @@ import csv
 robos = ["Betterment 72-28", "Betterment 90-10", "BrightPlan 60-40", "BrightPlan 90/10", "Fidelity 90-10", "Fidelity Portfolio", "FirstBridge HodgePodge(Jan 1, 2007)", 
         "FutureAdvisors Portfolio", "Int'l Market", "M-1 60-40", "M-1 90-10", "M-1 International SRI", "M-1 SRI", "New Schwab", "S&P 500", "Schwab 90-10", "Schwab Portfolio", 
         "SigFig 60-40", "SigFig 90-10", "SustainFolio P10", "SustainFolio P5", "TIAA 60-40", "TIAA 60-40 Impact Portfolio", "TIAA 90-10", "TIAA 90-10 Impact", 
-        "Three-Asset Portfolio (TAP 90-10)", "Three-Asset Portfolio 2(TAP 60-40)", "Vanguard 90-10", "Vanguard Portfolio", "Wealthfront 90-10", "Wealthfront Portfolio"]
+        "TAP 90-10", "TAP 60-40", "Vanguard 90-10", "Vanguard Portfolio", "Wealthfront 90-10", "Wealthfront Portfolio"]
 
 # short list of current robos being analyzed
-robos_sl = ["Betterment 90-10", "Fidelity 90-10", "Schwab 90-10", "SigFig 90-10", "Vanguard 90-10", "Wealthfront 90-10", "TIAA 90-10", "M-1 90-10", "Three-Asset Portfolio (TAP 90-10)", "Three-Asset Portfolio 2(TAP 60-40)",
+robos_sl = ["Betterment 90-10", "Fidelity 90-10", "Schwab 90-10", "SigFig 90-10", "Vanguard 90-10", "Wealthfront 90-10", "TIAA 90-10", "M-1 90-10", "TAP 90-10", "TAP 60-40",
             "Betterment 72-28", "Fidelity Portfolio", "FutureAdvisors Portfolio", "Schwab Portfolio", "SigFig 60-40", "Vanguard Portfolio", "Wealthfront Portfolio", "TIAA 60-40", "M-1 60-40", "BrightPlan 60-40"]
 
 def renameCSV():
